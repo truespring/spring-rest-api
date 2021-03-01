abb62853ce109a0f38d2c8671a01d1ad2d42426b
truespring <true_spring_@naver.com>
2021-03-01 18:05:38 +0900

    Swagger API 문서 자동화 기능 완료 localhost:8080/swagger-ui.html

--- src/main/java/com/truespring/restapi/controller/v1/UserController.java
+++ src/main/java/com/truespring/restapi/controller/v1/UserController.java
@@ -2,30 +2,35 @@ package com.truespring.restapi.controller.v1;
 
 import com.truespring.restapi.entity.User;
 import com.truespring.restapi.repo.UserJpaRepo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
 public class UserController {
+
     private final UserJpaRepo userJpaRepo;
 
+    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
     public List<User> findAllUser() {
         return userJpaRepo.findAll();
     }
 
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save() {
+    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
-                .uid("true_spring_@naver.com")
-                .name("석규환")
+                .uid(uid)
+                .name(name)
                 .build();
         return userJpaRepo.save(user);
     }
