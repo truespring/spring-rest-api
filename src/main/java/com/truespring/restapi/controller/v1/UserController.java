b9f07112b5ed3bafa87bc03b0f9848064a881ae3
truespring <true_spring_@naver.com>
2021-03-20 19:06:25 +0900

    복붙을 통한 ResponseBody 출력 결과 확인

--- src/main/java/com/truespring/restapi/controller/v1/UserController.java
+++ src/main/java/com/truespring/restapi/controller/v1/UserController.java
@@ -14,29 +14,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @Api(tags = {"1. User"})
 @RequiredArgsConstructor
-@RestController // 결과값을 JSON으로 출력
+@RestController
 @RequestMapping(value = "/v1")
 public class UserController {
 
     private final UserJpaRepo userJpaRepo;
     private final ResponseService responseService; // 결과를 처리할 Service
-    
-    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다.")
+
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/users")
     public ListResult<User> findAllUser() {
-        // 결과데이터가 여러건인 경우 getListResult를 이용해서 결과 출력
+        // 결과데이터가 여러건인경우 getListResult를 이용해서 결과를 출력한다.
         return responseService.getListResult(userJpaRepo.findAll());
     }
-
-//    @ApiOperation(value = "회원 단건 조회", notes = "userID로 회원을 조회한다")
-//    @GetMapping(value = "/user/{msrl}")
-//    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl, @ApiParam(value = "언어", defaultValue = "ko") @RequestParam String lang) {
-//        // 결과데이터가 단일건인 경우 getSingleResult를 이용해서 결과 출력
-//        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
-//    }
 
     @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
     @GetMapping(value = "/user/{msrl}")
@@ -45,7 +37,7 @@ public class UserController {
         return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
 
-    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다")
     @PostMapping(value = "/user")
     public SingleResult<User> save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
                                    @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
@@ -55,8 +47,8 @@ public class UserController {
                 .build();
         return responseService.getSingleResult(userJpaRepo.save(user));
     }
-    
-    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다.")
+
+    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다")
     @PutMapping(value = "/user")
     public SingleResult<User> modify(
             @ApiParam(value = "회원번호", required = true) @RequestParam long msrl,
@@ -69,12 +61,13 @@ public class UserController {
                 .build();
         return responseService.getSingleResult(userJpaRepo.save(user));
     }
-    
-    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다.")
+
+    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다")
     @DeleteMapping(value = "/user/{msrl}")
     public CommonResult delete(
             @ApiParam(value = "회원번호", required = true) @PathVariable long msrl) {
-        // 성공 결과 정보만 필요한 경우 getSuccessResult를 이용하여 결과 출력
+        userJpaRepo.deleteById(msrl);
+        // 성공 결과 정보만 필요한경우 getSuccessResult()를 이용하여 결과를 출력한다.
         return responseService.getSuccessResult();
     }
 }
